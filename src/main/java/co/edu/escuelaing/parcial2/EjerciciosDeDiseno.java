18416d6ecd25f2110b45fe99ea893e8b299a950a
Luis Amaya <luis.amaya-o@mail.escuelaing.edu.co>
2021-10-13 14:27:07 -0500

    DockerFile, Working in local

--- src/main/java/co/edu/escuelaing/parcial2/EjerciciosDeDiseno.java
+++ src/main/java/co/edu/escuelaing/parcial2/EjerciciosDeDiseno.java
@@ -2,6 +2,8 @@ package co.edu.escuelaing.parcial2;
 
 import static spark.Spark.*;
 
+import java.util.List;
+
 import org.json.JSONObject;
 
 import spark.Request;
@@ -20,14 +22,37 @@ public class EjerciciosDeDiseno {
             return null;
         });
 
-        get("/atan", "application/json", (req, res) -> operations(req, res));
+        get("/atan", "application/json", (req, res) -> {
+            res.type("application/json");
+            return operations("atan", req, res);
+        });
+        get("/sin", "application/json", (req, res) -> {
+            res.type("application/json");
+            return operations("sin", req, res);
+        });
 
     }
 
-    public static JSONObject operations(Request req, Response res) {
+    public static JSONObject operations(String operation, Request req, Response res) {
         JSONObject json;
+        Double value = Double.parseDouble(req.queryParams("value"));
+        Double result = 0.0;
+        if (operation.equals("atan")) {
+            result = cal.arTan(value);
+            json = getJSON(value, result, operation);
+        } else {
+            result = cal.sin(value);
+            json = getJSON(value, result, operation);
+        }
+        return json;
+    }
 
-        return null;
+    private static JSONObject getJSON(Double value, Double result, String operation) {
+        JSONObject json = new JSONObject();
+        json.put("Operation", operation);
+        json.put("Input", value);
+        json.put("Output", result);
+        return json;
     }
 
     public static int getPort() {
